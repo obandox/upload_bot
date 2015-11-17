61532e1956d3da26b7ed86a2f220079c93529121
miguel@kogimobile.com <miguel@kogimobile.com>
2015-11-17 02:50:23 -0500

    log file upload

--- bot.php
+++ bot.php
@@ -1,14 +1,29 @@
 <?php
 
+/**
+ * Primera prueba enviar 10 imagenes en una sola peticion
+ * $upload->sendImages(30, "single_01_", "Primera prueba enviar 10 imagenes en una sola peticion");
+ * Segunda prueba enviar 10 imagenes una tras otra
+ * $upload->sendRequests(30, 1, "stack_01_", "Segunda prueba enviar 10 imagenes una tras otra");
+ * Tercera prueba enviar 10 imagenes en paralelo
+ * $upload->sendRequestsParallel(30, 1, "parallel_01_", "Tercera prueba enviar 10 imagenes en paralelo");
+ */
+
 require "vendor/autoload.php";
 
 
 use PHPImageWorkshop\ImageWorkshop;
 
+ini_set('memory_limit','660M');
+set_time_limit(999999);
+
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Promise;
 
 $upload = new Uploader();
 
-$upload->generateTempImage("una imagen con datos");
+$upload->sendImages(4, "test_01_", "prueba enviar 4 imagenes en una sola peticion");
 
 
 class Uploader{
@@ -26,15 +41,52 @@ class Uploader{
             $this->images []= $filepath_base.$filename;
 		  } 
 		}
-
 	}
+
+    public function sendRequestsParallel($requestNumber, $imageNumber, $name, $content = null){
+        $this->sendRequests($requestNumber, $imageNumber, $name, $content, true);
+    }
+
+    public function sendRequests($requestNumber, $imageNumber, $name, $content = null, $future = false){
+        for ($i=0; $i < $requestNumber; $i++) { 
+            $this->sendImages($imageNumber, $i.'_'.$name, $content, $future);
+        }
+    }
+
+    public function sendImages($number, $name, $content = null, $future = false){
+
+        $multipart = [];
+
+        for ($i=0; $i < $number; $i++) { 
+               $filename = $name.'_'.$i;
+               $filePath = $this->generateTempImage($filename, $content);
+               $multipart []= [
+                    'name'     => $filename,
+                    'contents' => fopen($filePath, 'r')
+               ];
+        }
+
+        $client = new GuzzleHttp\Client();
+        $body = [
+            'multipart' => $multipart
+        ];
+        if($future){
+            $body["future"] = true;
+        }
+        echo "upload \n";
+        echo json_encode($body, JSON_PRETTY_PRINT)."\n";
+        $response = $client->request('POST', 'http://httpbin.org/post', $body);
+        echo json_encode($response, JSON_PRETTY_PRINT)."\n";
+    }
 
 	public function getRandomBase(){
         $image_path = $this->images[mt_rand(0, count($this->images) - 1)];
         return ImageWorkshop::initFromPath($image_path);
 	}
 
-	public function generateTempImage($name, $content = null){
+	public function generateTempImage($filename, $content = null){
+        $filename = $filename."_".rand(0,999).".png";
+
 		if($content == null) $content = date('Y-m-d h:i:s');
 
 		$group = $this->getRandomBase();
@@ -45,9 +97,9 @@ class Uploader{
 		$textRotation = 0;
 		$backgroundColor = null; // optionnal
 		
-        $shadow = ImageWorkshop::initTextLayer($name.":\n".$content, $fontPath, $fontSize+1, "000000", $textRotation, $backgroundColor);
+        $shadow = ImageWorkshop::initTextLayer($filename.":\n".$content, $fontPath, $fontSize+1, "000000", $textRotation, $backgroundColor);
 
-        $text = ImageWorkshop::initTextLayer($name.":\n".$content, $fontPath, $fontSize, $fontColor, $textRotation, $backgroundColor);
+        $text = ImageWorkshop::initTextLayer($filename.":\n".$content, $fontPath, $fontSize, $fontColor, $textRotation, $backgroundColor);
 
 		$level = 2; 
 		$positionX = 80; 
@@ -60,12 +112,12 @@ class Uploader{
 
 
 		$dirPath = $this->dirBase."/temp/";
-		$filename = $name."_".time().".png";
 		$createFolders = true;
 		$backgroundColor = null; // transparent, only for PNG (otherwise it will be white if set null)
 		$imageQuality = 95; // useless for GIF, usefull for PNG and JPEG (0 to 100%)
 		 
 		$group->save($dirPath, $filename, $createFolders, $backgroundColor, $imageQuality);
+        echo $dirPath.$filename."\n";
         return $dirPath.$filename;
 	}
 
